659c7e6d8010d5674cb3d0ee013b2e903ecd81eb
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2021-11-20 20:07:30 +0300

    add connection dialog

---
+++ src/qgis_stac/gui/connection_dialog.py
@@ -0,0 +1,19 @@
+import os
+from qgis.PyQt import QtWidgets
+from qgis.PyQt.uic import loadUiType
+
+
+DialogUi, _ = loadUiType(
+    os.path.join(os.path.dirname(__file__), "../ui/connection_dialog.ui")
+)
+
+
+class ConnectionDialog(QtWidgets.QDialog, DialogUi):
+    def __init__(
+            self,
+            parent=None,
+    ):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(False)
+        self.buttonBox.button(QtWidgets.QDialogButtonBox.Cancel).clicked.connect(self.reject)
