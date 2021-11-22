1850575fb00052682fd1863d4c8522a25a987187
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2021-11-22 19:33:16 +0300

    review PR

--- src/qgis_stac/gui/connection_dialog.py
+++ src/qgis_stac/gui/connection_dialog.py
@@ -16,3 +16,14 @@ class ConnectionDialog(QtWidgets.QDialog, DialogUi):
         super().__init__(parent)
         self.setupUi(self)
         self.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(False)
+
+        ok_signals = [
+            self.name_edit.textChanged,
+            self.url_edit.textChanged,
+        ]
+        for signal in ok_signals:
+            signal.connect(self.update_ok_buttons)
+
+    def update_ok_buttons(self):
+        enabled_state = self.name_edit.text() != "" and self.url_edit.text() != ""
+        self.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(enabled_state)
