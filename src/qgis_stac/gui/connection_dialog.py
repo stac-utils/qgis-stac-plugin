e3860dc8320f58628d43baea7c7c4f5583f81104
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2021-11-27 23:03:06 +0300

    add connection

--- src/qgis_stac/gui/connection_dialog.py
+++ src/qgis_stac/gui/connection_dialog.py
@@ -1,7 +1,13 @@
 import os
+import re
+import uuid
+import typing
 from qgis.PyQt import QtWidgets
 from qgis.PyQt.uic import loadUiType
-
+from ..conf import (
+    ConnectionSettings,
+    settings_manager
+)
 
 DialogUi, _ = loadUiType(
     os.path.join(os.path.dirname(__file__), "../ui/connection_dialog.ui")
@@ -9,11 +15,12 @@ DialogUi, _ = loadUiType(
 
 
 class ConnectionDialog(QtWidgets.QDialog, DialogUi):
-    def __init__(
-            self,
-            parent=None,
-    ):
-        super().__init__(parent)
+    connection_id: uuid.UUID
+
+    def __init__(self,
+                 connection_settings: typing.Optional[ConnectionSettings] = None
+                 ):
+        super().__init__()
         self.setupUi(self)
         self.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(False)
 
@@ -23,6 +30,46 @@ class ConnectionDialog(QtWidgets.QDialog, DialogUi):
         ]
         for signal in ok_signals:
             signal.connect(self.update_ok_buttons)
+        if connection_settings is not None:
+            self.connection_id = connection_settings.id
+            self.load_connection_settings(connection_settings)
+        else:
+            self.connection_id = uuid.uuid4()
+
+    def load_connection_settings(self, connection_settings: ConnectionSettings):
+        self.name_edit.setText(connection_settings.name)
+        self.url_edit.setText(connection_settings.base_url)
+        self.auth_config.setConfigId(connection_settings.auth_config)
+        self.page_size.setValue(connection_settings.page_size)
+
+    def get_connection_settings(self) -> ConnectionSettings:
+
+        return ConnectionSettings(
+            id=self.connection_id,
+            name=self.name_edit.text().strip(),
+            url=self.url_edit.text().strip(),
+            page_size=self.page_size.value(),
+            auth_config=self.auth_config.configId(),
+        )
+
+    def accept(self):
+        connection_settings = self.get_connection_settings()
+        name_pattern = re.compile(
+            f"^{connection_settings.name}$|^{connection_settings.name}(\(\d+\))$"
+        )
+        duplicate_names = []
+        for connection_conf in settings_manager.list_connections():
+            if connection_conf.id == connection_settings.id:
+                continue  # we don't want to compare against ourselves
+            if name_pattern.search(connection_conf.name) is not None:
+                duplicate_names.append(connection_conf.name)
+        if len(duplicate_names) > 0:
+            connection_settings.name = (
+                f"{connection_settings.name}({len(duplicate_names)})"
+            )
+        settings_manager.save_connection_settings(connection_settings)
+        settings_manager.set_current_connection(connection_settings.id)
+        super().accept()
 
     def update_ok_buttons(self):
         enabled_state = self.name_edit.text() != "" and self.url_edit.text() != ""
--- src/qgis_stac/gui/main.py
+++ src/qgis_stac/gui/main.py
@@ -3,6 +3,7 @@ import os
 from qgis.PyQt import QtCore, QtGui, QtNetwork, QtWidgets, QtXml
 from qgis.PyQt.uic import loadUiType
 
+from ..conf import settings_manager
 from ..resources import *
 from ..gui.connection_dialog import ConnectionDialog
 
@@ -14,6 +15,9 @@ WidgetUi, _ = loadUiType(
 class QgisStacWidget(QtWidgets.QWidget, WidgetUi):
     new_connection_btn: QtWidgets.QPushButton
     pagination: QtWidgets.QWidget
+    connections_cmb: QtWidgets.QComboBox
+    edit_connection_btn: QtWidgets.QPushButton
+    delete_connection_btn: QtWidgets.QPushButton
 
     def __init__(
             self,
@@ -22,8 +26,38 @@ class QgisStacWidget(QtWidgets.QWidget, WidgetUi):
         super().__init__(parent)
         self.setupUi(self)
         self.new_connection_btn.clicked.connect(self.add_connection)
+        self.connections_cmb.currentIndexChanged.connect(
+            self.toggle_connection_management_buttons
+        )
+        self.update_connections_combobox()
+        self.toggle_connection_management_buttons()
+        self.connections_cmb.activated.connect(self.update_current_connection)
         self.pagination.setVisible(False)
 
     def add_connection(self):
         connection_dialog = ConnectionDialog()
         connection_dialog.exec_()
+        self.update_connections_combobox()
+
+    def toggle_connection_management_buttons(self):
+        current_name = self.connections_cmb.currentText()
+        enabled = current_name != ""
+        self.edit_connection_btn.setEnabled(enabled)
+        self.delete_connection_btn.setEnabled(enabled)
+
+    def update_connections_combobox(self):
+        existing_connections = settings_manager.list_connections()
+        self.connections_cmb.clear()
+        if len(existing_connections) > 0:
+            self.connections_cmb.addItems(conn.name for conn in existing_connections)
+            current_connection = settings_manager.get_current_connection()
+            if current_connection is not None:
+                current_index = self.connections_cmb.findText(current_connection.name)
+                self.connections_cmb.setCurrentIndex(current_index)
+            else:
+                self.connections_cmb.setCurrentIndex(0)
+
+    def update_current_connection(self, current_index: int):
+        current_text = self.connections_cmb.itemText(current_index)
+        current_connection = settings_manager.find_connection_by_name(current_text)
+        settings_manager.set_current_connection(current_connection.id)
