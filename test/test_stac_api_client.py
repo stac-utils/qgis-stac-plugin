f4afe10e06cd4af62bcf11552b0f522bee814268
Samweli <samweli@kartoza.com>
2021-12-30 01:10:29 +0300

    test resource fetching in one function

--- test/test_stac_api_client.py
+++ test/test_stac_api_client.py
@@ -19,7 +19,8 @@ class STACApiClientTest(unittest.TestCase):
         self.api_client = Client(self.server.url)
         self.response = None
 
-    def test_items_search(self):
+    def test_resources_fetch(self):
+        # check items searching
         spy = QSignalSpy(self.api_client.items_received)
         self.api_client.items_received.connect(self.app_response)
         self.api_client.get_items(ItemSearch(collections=['simple-collection']))
@@ -34,6 +35,25 @@ class STACApiClientTest(unittest.TestCase):
         self.assertEqual(len(items), 2)
         self.assertEqual(items[0].id, "20201211_223832_CS1")
         self.assertEqual(items[1].id, "20201211_223832_CS2")
+
+        # check conformance fetching
+        spy = QSignalSpy(self.api_client.conformance_received)
+        self.api_client.conformance_received.connect(self.app_response)
+        self.api_client.get_conformance()
+        result = spy.wait(timeout=1000)
+
+        self.assertTrue(result)
+        self.assertIsNotNone(self.response)
+        self.assertEqual(len(self.response), 2)
+
+        conformance_classes = self.response[0]
+
+        self.assertEqual(len(conformance_classes), 16)
+        self.assertEqual(conformance_classes[0].name, 'core')
+        self.assertEqual(
+            conformance_classes[0].uri,
+            "http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/core"
+        )
 
     # TODO resolve self href error when iterating over collections
     # def test_collections_search(self):
@@ -52,25 +72,6 @@ class STACApiClientTest(unittest.TestCase):
     #     self.assertEqual(len(collections), 1)
     #     self.assertEqual(collections[0].id, "simple-collection")
     #     self.assertEqual(collections[0].title, "Simple Example Collection")
-
-    def test_conformance_fetch(self):
-        spy = QSignalSpy(self.api_client.conformance_received)
-        self.api_client.conformance_received.connect(self.app_response)
-        self.api_client.get_conformance()
-        result = spy.wait(timeout=1000)
-
-        self.assertTrue(result)
-        self.assertIsNotNone(self.response)
-        self.assertEqual(len(self.response), 2)
-
-        conformance_classes = self.response[0]
-
-        self.assertEqual(len(conformance_classes), 16)
-        self.assertEqual(conformance_classes[0].name, 'core')
-        self.assertEqual(
-            conformance_classes[0].uri,
-            "http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/core"
-        )
 
     def app_response(self, *response_args):
         self.response = response_args
