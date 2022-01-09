8b334efc80b622abbb9674a32d9ad8b943c7d393
Samweli <samweli@kartoza.com>
2022-01-09 03:55:41 +0300

    update client libs in order to catch libs exceptions in the plugin

--- src/qgis_stac/gui/qgis_stac_widget.py
+++ src/qgis_stac/gui/qgis_stac_widget.py
@@ -120,6 +120,8 @@ class QgisStacWidget(QtWidgets.QDialog, WidgetUi):
         self.update_connection_buttons()
         self.connections_box.activated.connect(self.update_current_connection)
 
+        self.search_error_message = None
+
         # initialize page
         self.page = 1
         self.total_pages = 0
@@ -428,6 +430,9 @@ class QgisStacWidget(QtWidgets.QDialog, WidgetUi):
     def handle_search_end(self):
         """ Handles the logic to be executed when searching has ended"""
         self.message_bar.clearWidgets()
+        if self.search_error_message:
+            self.show_message(self.search_error_message, Qgis.Critical)
+            self.search_error_message = None
         self.update_search_inputs(enabled=True)
 
     def update_search_inputs(self, enabled):
@@ -539,13 +544,16 @@ class QgisStacWidget(QtWidgets.QDialog, WidgetUi):
 
     def display_search_error(self, message):
         """
-        Show the search error message.
+        Shows the search error message.
+        Sets the search error message and
+        emits search_completed signal that alerts the search end handler to
+        display the search error message.
 
         :param message: search error message.
         :type message: str
         """
         self.message_bar.clearWidgets()
-        self.show_message(message, level=Qgis.Critical)
+        self.search_error_message = message
         self.search_completed.emit()
 
     def populate_results(self, results):
@@ -682,8 +690,10 @@ class QgisStacWidget(QtWidgets.QDialog, WidgetUi):
                 len(collections)
             )
         )
+
         for result in collections:
-            item = QtGui.QStandardItem(result.title)
+            title = result.title if result.title else tr("No Title")
+            item = QtGui.QStandardItem(title)
             item.setData(result.id, 1)
             self.model.appendRow(item)
 
